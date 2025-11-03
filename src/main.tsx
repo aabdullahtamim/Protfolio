f2e3cf176390141b22f88a44eb3bf53da0dd6319
aabdullahtamim <atwebofficial@gmail.com>
2025-11-03 15:12:22 +0600

    Custom Cursor Edit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from '@/components/router/router'
-import ParticleOrbitEffect from '@/components/ui/particle-orbit-effect'
+// import ParticleOrbitEffect from '@/components/ui/particle-orbit-effect'
+import CustomCursor from '@/components/ui/CustomCursor'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ParticleOrbitEffect />
+    <CustomCursor />
     <RouterProvider router={router} />,
   </StrictMode>,
 )
